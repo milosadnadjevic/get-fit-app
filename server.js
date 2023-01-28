11e108ec130c5fd6c36efbe5e3c0ab2942179f9f
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-01-28 16:01:24 -0500

    server created

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 // dependencies
 const express = require('express')
+const mongoose = require('mongoose')
 
 // initialize the app
 const app = express()
 
 // configure settings
+
 require('dotenv').config()
 const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+
 // establish connection to mongodb
+mongoose.set('strictQuery', false)
+mongoose.connect(DATABASE_URL)
+
+const db = mongoose.connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB')
+})
+db.on('error', (error) => {
+    console.log('An error occured with MongoDB' + error.message)
+})
 
 // mount middleware
 
